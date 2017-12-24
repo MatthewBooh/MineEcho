31d084fcb102bf7ce61576defcd78fee33a01951
MatthewBooh <34801807+MatthewBooh@users.noreply.github.com>
2017-12-24 10:53:35 +1000

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const config = require("./config.json");
+
+client.on('ready', () => {
+  console.log(`Started.`);
+  client.user.setGame(config.status);
+});
+
+client.on('message', message => {
+
+  if (message.content.toLowerCase() === config.prefix + `about`) {
+    message.reply(`About test, blah blah blah..`);
+  }
+  
+});
+
+client.login(config.token);
