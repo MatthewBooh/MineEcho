d1ae10b9865e73a8a9939c2a07ae94ffb0d00f66
MatthewBooh <34801807+MatthewBooh@users.noreply.github.com>
2017-12-24 11:09:53 +1000

    Start beta work on receiving messages

--- index.js
+++ index.js
@@ -2,10 +2,34 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 const config = require("./config.json");
+const mc = require('minecraft-protocol');
 
 client.on('ready', () => {
-  console.log(`Started.`);
+  console.log(`Initiating startup..`);
   client.user.setGame(config.status);
+  console.log(`Set bot user status to config.status.`);
+  console.log(`Attempting to connect to the server client..`);
+  var serverclient = mc.createClient({
+    host: config.host,
+    port: config.port,
+    username: config.email,
+    password: config.password,
+  });
+  if (!serverclient) {
+    console.log(`Server client could not be connected.\nPlease fix any details in your config.json and reboot the bot.`);
+  } else { console.log(`Server client connected.`); console.log(`Startup finished.`); }
+});
+
+client.on('chat', function(packet) {
+  
+  var jsonMsg = JSON.parse(packet.message);
+  if(jsonMsg.translate == 'chat.type.announcement' || jsonMsg.translate == 'chat.type.text') {
+    var username = jsonMsg.with[0].text;
+    var msg = jsonMsg.with[1];
+    // if(username === client.username) return;
+    // client.write('chat', {message: msg});
+    console.log(username + `: ` + msg);
+  }
 });
 
 client.on('message', message => { if (!message.content.startsWith(config.prefix) || message.author.bot) return;
